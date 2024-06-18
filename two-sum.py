36d3ef4af4e2a979b5d0cefeb0ed4c54ea08bb00
yukik8 <andres8.yuuki@gmail.com>
2024-06-18 21:22:27 +0900

    Two Sum 1 2024/06/18

---
+++ two-sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        for i in range(len(nums)-1):
+            for j in range(i+1, len(nums)):
+                if nums[i] + nums[j] == target:
+                    return [i,j]
+        return 'No solution'
+
+
+nums = input('nums = ')
+inputnums = [int(item) for item in nums.split(',')]
+target = int(input('target = '))
+answer = Solution().twoSum(inputnums, target)
+print(answer)
