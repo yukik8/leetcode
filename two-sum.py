b5e9ead497286e98a2d17d8ab9cc4d13893d2962
yukik8 <andres8.yuuki@gmail.com>
2024-06-18 22:22:18 +0900

    LeetCodeエラー箇所の明記

--- two-sum.py
+++ two-sum.py
@@ -1,10 +1,5 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
         for i in range(len(nums)-1):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
@@ -13,7 +8,7 @@ class Solution(object):
 
 
 nums = input('nums = ')
-inputnums = [int(item) for item in nums.split(',')]
+inputnums = [int(item) for item in nums.split(',')] #LeetCode上でエラー発生
 target = int(input('target = '))
 answer = Solution().twoSum(inputnums, target)
 print(answer)
